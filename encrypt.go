5d6bac3f9efb6ef7e773f95d970a9f1093158dd0
Jim McDonald <Jim@mcdee.net>
2023-07-29 14:28:02 +0100

    Increase test coverage for encryption.

--- encrypt.go
+++ encrypt.go
@@ -22,31 +22,47 @@ import (
 // encryptIfRequired encrypts data if required.
 func (s *Store) encryptIfRequired(data []byte) ([]byte, error) {
 	if len(data) == 0 {
+		// No data means nothing to encrypt.
 		return data, nil
 	}
+
+	if len(s.passphrase) == 0 {
+		// No passphrase means nothing to encrypt with.
+		return data, nil
+	}
+
 	if len(data) < 16 {
 		return nil, errors.New("data must be at least 16 bytes")
 	}
+
 	var err error
-	if len(s.passphrase) > 0 {
-		data, err = ecodec.Encrypt(data, s.passphrase)
+	if data, err = ecodec.Encrypt(data, s.passphrase); err != nil {
+		return nil, err
 	}
 
-	return data, err
+	return data, nil
 }
 
 // decryptIfRequired decrypts data if required.
 func (s *Store) decryptIfRequired(data []byte) ([]byte, error) {
 	if len(data) == 0 {
+		// No data means nothing to decrypt.
 		return data, nil
 	}
+
+	if len(s.passphrase) == 0 {
+		// No passphrase means nothing to decrypt with.
+		return data, nil
+	}
+
 	if len(data) < 16 {
 		return nil, errors.New("data must be at least 16 bytes")
 	}
+
 	var err error
-	if len(s.passphrase) > 0 {
-		data, err = ecodec.Decrypt(data, s.passphrase)
+	if data, err = ecodec.Decrypt(data, s.passphrase); err != nil {
+		return nil, err
 	}
 
-	return data, err
+	return data, nil
 }
